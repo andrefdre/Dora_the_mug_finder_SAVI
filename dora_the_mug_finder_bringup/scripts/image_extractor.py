f1f07e380eee1c53e59b5fee4ab4a16451a2ed38
Andre <andref@ua.pt>
2023-01-17 16:02:49 +0000

    displays the images with matplotlib #22

--- dora_the_mug_finder_bringup/scripts/image_extractor.py
+++ dora_the_mug_finder_bringup/scripts/image_extractor.py
@@ -15,68 +15,107 @@ import numpy as np
 import glob
 import os
 import cv2
+import matplotlib.pyplot as plt
 
 import rospy
 from dora_the_mug_finder_msg.msg import Object , Point
 
 
-def callback(data):
-    files_path=f'{os.environ["DORA"]}'
+class Image:
+
+    def __init__(self):
+        self.figure = plt.figure("Cropped Images")
+        self.figure.set_size_inches(10,5)
+        self.cropped_images = []
+
+    def callback(self,data):
+        files_path=f'{os.environ["DORA"]}'
+        
+        # Scene dataset paths
+        filenames = []
+        filenames.append (files_path + '/rgbd-scenes-v2/imgs/scene_03/00000-color.png')
+
+        points = np.array([[center.x,center.y,center.z] for center in data.center],dtype = np.float64)
+        bbox_3d =np.array( [[[data.corners[idx].x,data.corners[idx+1].y+0.05,data.corners[idx].z],[data.corners[idx+1].x,data.corners[idx].y,data.corners[idx].z]] for idx in range(0,len(data.corners),2)] ,dtype=np.float64)                    
+        
+
+        # Camera parameters
+        center = [320 , 240]
+        focal_length = 570.3
+        MM_PER_M = 1000
+
+
+        camera_matrix = np.array([[focal_length, 0,            center[0]],
+                                [0,            focal_length, center[1]],
+                                [0,            0,            1]])
+
+        # Project the 3D points to the 2D image plane
+        points_2d = cv2.projectPoints(points, np.identity(3), np.zeros(3), camera_matrix, None,)[0]
+
+        bbox_2d = []
+
+        for corners in bbox_3d:
+            bbox_2d.append(cv2.projectPoints(corners, np.identity(3), np.zeros(3), camera_matrix, None,)[0])
+
+        # Scale the points to image pixels
+        points_2d = np.round(points_2d).astype(int)
+        bbox_2d = np.round(bbox_2d).astype(int)
+
+        # Blue color in BGR
+        color = (0, 251, 255)
     
-    # Scene dataset paths
-    filenames = []
-    filenames.append (files_path + '/rgbd-scenes-v2/imgs/scene_03/00000-color.png')
+        # Line thickness of 2 px
+        thickness = 2
+        self.cropped_images = []
+        for filename in filenames:
+            image = cv2.imread(filename)
+            image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
 
-    points = np.array([[center.x,center.y,center.z] for center in data.center],dtype = np.float64)
-    bbox_3d =np.array( [[[data.corners[idx].x,data.corners[idx+1].y+0.05,data.corners[idx].z],[data.corners[idx+1].x,data.corners[idx].y,data.corners[idx].z]] for idx in range(0,len(data.corners),2)] ,dtype=np.float64)                    
-    
+            for idx,point_2d in enumerate(points_2d):
+                image[point_2d[0][1]-2:point_2d[0][1]+2,point_2d[0][0]-2:point_2d[0][0]+2]=color
+                image = cv2.rectangle(image, bbox_2d[idx][0][0], bbox_2d[idx][1][0], color, thickness)
+                cropped_image = image[bbox_2d[idx][1][0][1]:bbox_2d[idx][0][0][1],bbox_2d[idx][0][0][0]:bbox_2d[idx][1][0][0]]
+                self.cropped_images.append(cropped_image)
 
-    # Camera parameters
-    center = [320 , 240]
-    focal_length = 570.3
-    MM_PER_M = 1000
+        print("loop")
 
-
-    camera_matrix = np.array([[focal_length, 0,            center[0]],
-                              [0,            focal_length, center[1]],
-                              [0,            0,            1]])
-
-    # Project the 3D points to the 2D image plane
-    points_2d = cv2.projectPoints(points, np.identity(3), np.zeros(3), camera_matrix, None,)[0]
-
-    bbox_2d = []
-
-    for corners in bbox_3d:
-        bbox_2d.append(cv2.projectPoints(corners, np.identity(3), np.zeros(3), camera_matrix, None,)[0])
-
-    # Scale the points to image pixels
-    points_2d = np.round(points_2d).astype(int)
-    bbox_2d = np.round(bbox_2d).astype(int)
-
-    # Blue color in BGR
-    color = (0, 251, 255)
+    def draw(self):
+        plt.clf()
+        for image_idx,cropped_image in enumerate(self.cropped_images,start=1):
+            ax = self.figure.add_subplot(2,5,image_idx) # define a 5 x 5 subplot matrix
+            plt.imshow(cropped_image)
+            ax.xaxis.set_ticklabels([])
+            ax.yaxis.set_ticklabels([])
+            ax.xaxis.set_ticks([])
+            ax.yaxis.set_ticks([])
+            ax.set_xlabel(str(image_idx), color='green')
   
-    # Line thickness of 2 px
-    thickness = 2
 
-    for filename in filenames:
-        image = cv2.imread(filename)
+        
+        # plt.show(block=False)
+        # plt.pause(1)
+        # plt.close()
+        plt.draw()
 
-        for idx,point_2d in enumerate(points_2d):
-            image[point_2d[0][1]-2:point_2d[0][1]+2,point_2d[0][0]-2:point_2d[0][0]+2]=color
-            image = cv2.rectangle(image, bbox_2d[idx][0][0], bbox_2d[idx][1][0], color, thickness)
-      
-    cv2.imshow("window" , image)
-    cv2.waitKey(1)
+        key = plt.waitforbuttonpress(1)
+        if not plt.fignum_exists(1):
+            print('Terminating')
+            exit(0)
 
 
 def main():
 
     rospy.init_node('image_extractor', anonymous=False)
 
-    rospy.Subscriber("objects_publisher", Object, callback)
+    image = Image()
 
-    rospy.spin()
+    rospy.Subscriber("objects_publisher", Object, image.callback)
+
+    while not rospy.is_shutdown():
+        print("hello")
+        image.draw()
+        rospy.sleep(1) 
+        
 
 
 if __name__ == "__main__":
