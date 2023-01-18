e6c17a8fb1be7203a34b622fc472c35c5eef055a
Andre <andref@ua.pt>
2023-01-18 19:21:18 +0000

    implemented visualizer #33

--- dora_the_mug_finder_bringup/scripts/image_extractor.py
+++ dora_the_mug_finder_bringup/scripts/image_extractor.py
@@ -39,7 +39,7 @@ class Image:
         
         # Scene dataset paths
         filenames = []
-        filenames.append (files_path + '/rgbd-scenes-v2/imgs/scene_03/00000-color.png')
+        filenames.append (files_path + '/rgbd-scenes-v2/imgs/scene_05/00000-color.png')
 
         points = np.array([[center.x,center.y,center.z] for center in data.center],dtype = np.float64)
         bbox_3d =np.array( [[[data.corners[idx].x,data.corners[idx+1].y+0.05,data.corners[idx].z],[data.corners[idx+1].x,data.corners[idx].y,data.corners[idx].z]] for idx in range(0,len(data.corners),2)] ,dtype=np.float64)                    
